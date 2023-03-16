84b926b979d62eca504b36422d05d6465b7c9609
Cyber-Globe <ojeikelvin@gmail.com>
2023-03-16 13:29:58 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a line of c using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
