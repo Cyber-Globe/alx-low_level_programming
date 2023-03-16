a81f0e97ef50a5971a3d1752dc5b5ddf0b3dac33
Cyber-Globe <ojeikelvin@gmail.com>
2023-03-16 13:38:24 -0700

    5-printf.c completed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print a line of c using printf function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
+
