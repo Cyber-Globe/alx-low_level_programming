3f7070ae9c91651834d2886171d69d9292024634
Cyber-Globe <ojeikelvin@gmail.com>
2023-03-17 21:51:33 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print numberz
+ *
+ * Return: alwarys 0 (Success)
+ */
+int main(void)
+{
+	char i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
