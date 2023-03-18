8d30d2f00177705f69771a86da0d9fbde96318d0
Cyber-Globe <ojeikelvin@gmail.com>
2023-03-17 21:25:50 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the alphabet in this order
+ *
+ * Return. Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
