470d972ecdc0c4e858d37ca9b6e1e44728c4cafb
Cyber-Globe <ojeikelvin@gmail.com>
2023-03-23 02:11:20 -0700

    0. Multiple mains mandatory In most projects, we often give you only one main file to test with. For example, this main file is a test for a postitive_or_negative() function similar to the one you worked with in an earlier C project:

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative - determines if a number is positive or negative
+ * @i: integer
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+
+
+}
