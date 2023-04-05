8e46ba0bb67fbbe35f9cb2382ead0359ea197484
Cyber-Globe <ojeikelvin@gmail.com>
2023-04-05 05:48:18 -0700

    task edited and rreused

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,14 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (*s)
 	{
-		_putchar(s[i]);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+
+	else
+		_putchar('\n');
 }
+
 
