d965f98f043561741b6a4220b91bd0687f20f1f5
Cyber-Globe <ojeikelvin@gmail.com>
2023-04-07 19:28:55 -0700

    task completed

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *main -  entry poin for the code
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	_puts("\"At the end of the day,\"\n\t- Kevin Mitnick");
+	return (0);
+}
