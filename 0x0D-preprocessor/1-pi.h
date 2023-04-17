0e80268b97e486c8b5abd416c22041240104d397
Cyber-Globe <ojeikelvin@gmail.com>
2023-04-17 09:08:16 -0700

    task 1 completed

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h is the header first file
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
+
